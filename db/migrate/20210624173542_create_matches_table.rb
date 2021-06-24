e27ae6ab6193758e2bddee38f86a602fca94bca1
denis.rulev <denis.rulev@balance-pl.ru>
2021-06-24 22:04:48 +0300

    Деплой на хероку

---
+++ db/migrate/20210624173542_create_matches_table.rb
@@ -0,0 +1,19 @@
+class CreateMatchesTable < ActiveRecord::Migration[5.2]
+  def change
+    drop_table(:matches, force: true)
+    create_table :matches do |t|
+      t.integer :player_id_1
+      t.integer :player_id_2
+      t.integer :player1_set_1
+      t.integer :player1_set_2
+      t.integer :player1_set_3
+      t.integer :player2_set_1
+      t.integer :player2_set_2
+      t.integer :player2_set_3
+      t.datetime :start_time
+      t.integer :winner
+
+      t.timestamps
+    end
+  end
+end
